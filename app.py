13b1ad6a46c0c6da8d4b70aaec449286cd5f1968
Aditya Raj <rajaditya079@gmail.com>
2025-04-18 09:16:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/publications')
+def publications():
+    return render_template('publications.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contactme')
+def contactme():
+    return render_template('contactme.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
